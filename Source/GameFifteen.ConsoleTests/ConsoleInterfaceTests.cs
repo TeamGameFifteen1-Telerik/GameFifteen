7c432cb9ab3a713389226c1ad745e459de15681a
M-Yankov <pointblank12345@abv.bg>
2015-10-08 01:15:08 +0300

    Itial Unit test

---
+++ Source/GameFifteen.ConsoleTests/ConsoleInterfaceTests.cs
@@ -0,0 +1,22 @@
+﻿namespace GameFifteen.ConsoleTests
+{
+    using System;
+    using System.IO;
+    using GameFifteen.Console;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class ConsoleInterfaceTests
+    {
+        [TestMethod]
+        public void TestMethod2()
+        {
+            StreamReader reader = new StreamReader("..//..//testCommands.txt");
+            Console.SetIn(reader);
+            ConsoleInterface interf = new ConsoleInterface();
+            string result = interf.GetUserInput();
+            result = interf.GetUserInput();
+            Assert.AreEqual("solve", result);
+        }
+    }
+}
