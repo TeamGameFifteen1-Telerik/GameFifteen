3ee8ade66eb7319610f77834da0c79aad8748068
h.slavov <slavov.hristo@gmail.com>
2015-10-11 01:56:42 +0300

    Added GameStarterTests.cs

---
+++ Source/GameFifteen.ConsoleTests/GameStarterTests.cs
@@ -0,0 +1,26 @@
+﻿namespace GameFifteen.LogicTests
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using GameFifteen.Console;
+
+    /// <summary>
+    /// Class tests GameStarter functionalities.
+    /// </summary>
+    [TestClass]
+    public class GameStarterTests
+    {
+        /// <summary>
+        /// Method tests if GameStarter instance is unique by the hash-code of instances.
+        /// </summary>
+        [TestMethod]
+        public void TestGameStarterInstanceToBeUnique()
+        {
+            var firstInstance = GameFifteenStarter.Instance;
+            var secondInstance = GameFifteenStarter.Instance;
+            var expected = firstInstance.GetHashCode();
+            var actual = secondInstance.GetHashCode();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
