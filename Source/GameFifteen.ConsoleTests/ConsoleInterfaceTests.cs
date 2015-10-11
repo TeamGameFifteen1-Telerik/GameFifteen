1b6ef646c1c4b4dea56070760cc95eebcd0fb972
M-Yankov <pointblank12345@abv.bg>
2015-10-11 14:56:53 +0300

    Removed Stream readers from tests.

--- Source/GameFifteen.ConsoleTests/ConsoleInterfaceTests.cs
+++ Source/GameFifteen.ConsoleTests/ConsoleInterfaceTests.cs
@@ -2,6 +2,8 @@
 {
     using System;
     using System.IO;
+    using System.Linq;
+    using System.Collections.Generic;
     using GameFifteen.Common;
     using GameFifteen.Console;
     using GameFifteen.Logic;
@@ -15,136 +17,150 @@
         [TestMethod]
         public void TestGetUserInputToReturnCorrectValue()
         {
-            StreamReader reader = new StreamReader("..//..//testCommands.txt");
-            Console.SetIn(reader);
-            ConsoleInterface interf = new ConsoleInterface();
-            string result = interf.GetUserInput();
-            result = interf.GetUserInput();
-            Assert.AreEqual("solve", result);
+            var mockedReader = new Mock<HelperReader>();
+            Console.SetIn(mockedReader.Object);
+
+            IList<string> input = new List<string>() { "start", "solve", "pesho", "yes", "solve", "pesho", "no " };
+
+            int indexOfCommand = 0;
+            mockedReader.Setup(r => r.ReadLine()).Returns(() => input[indexOfCommand]);
+
+            IUserInterface interf = new ConsoleInterface();
+            string result;
+            for (; indexOfCommand < input.Count; indexOfCommand++)
+            {
+                result = interf.GetUserInput();
+                Assert.AreEqual(input[indexOfCommand], result);
+            }
+
+            mockedReader.Verify(r => r.ReadLine(), Times.Exactly(input.Count));
         }
 
         [TestMethod]
         [ExpectedException(typeof(ArgumentException), "No such argument")]
         public void TestToThrowExceptonWhenInvalidArgumentIsPassed()
         {
-            ConsoleInterface interf = new ConsoleInterface();
+            IUserInterface interf = new ConsoleInterface();
             dynamic result = interf.GetArgumentValue("InvlaidArgument");
         }
 
         [TestMethod]
         public void TestToGetArgumnetValueReturnCorectValue()
         {
-            StreamReader reader = new StreamReader("../../styleCommands.txt");
-            Console.SetIn(reader);
+            var mockedReader = new Mock<HelperReader>();
+            Console.SetIn(mockedReader.Object);
 
-            ConsoleInterface interf = new ConsoleInterface();
+            IList<string> styleCommands =
+                new List<string>() { "style=dotted", "style=solid", "style=fat", "style=double" };
+
+            int indexOfCommands = 0;
+            mockedReader.Setup(r => r.ReadLine()).Returns(() => styleCommands[indexOfCommands]);
+
+            IUserInterface interf = new ConsoleInterface();
             dynamic result;
-
-            var styleCommands = reader.ReadToEnd().Split(new string[] { "\r\n", "style=" }, StringSplitOptions.RemoveEmptyEntries);
-
-            //// reset reader to get input again
-            reader.DiscardBufferedData();
-            reader.BaseStream.Seek(0, SeekOrigin.Begin);
-            reader.BaseStream.Position = 0;
             bool actual;
 
-            for (int i = 0; i < styleCommands.Length; i++)
+            for (; indexOfCommands < styleCommands.Count; indexOfCommands++)
             {
                 interf.GetCommandFromInput();
                 result = interf.GetArgumentValue(GlobalConstants.GridBorderStyle);
-                actual = (result as string == styleCommands[i]);
+                actual = (result as string == styleCommands[indexOfCommands].Replace("style=", string.Empty));
                 Assert.IsTrue(actual);
             }
+
+            mockedReader.Verify(r => r.ReadLine(), Times.Exactly(styleCommands.Count));
         }
 
         [TestMethod]
         public void TestGetArgumentToReturnCorrectNumberToMove()
         {
-            StreamReader reader = new StreamReader("../../testCommands/Commandmoves.txt");
-            Console.SetIn(reader);
+            var mockedReader = new Mock<HelperReader>();
+            Console.SetIn(mockedReader.Object);
 
-            ConsoleInterface interf = new ConsoleInterface();
+            IList<int> moveCommands = new List<int>() { 1, 2, 2, 3, 4, 58, 1001, 60123, 1231, 123, 12, 31, 312, 3131312, 123, 1 };
+
+            IUserInterface interf = new ConsoleInterface();
             dynamic result;
 
-            var numberCommands = reader.ReadToEnd().Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
-
-            reader.DiscardBufferedData();
-            reader.BaseStream.Seek(0, SeekOrigin.Begin);
-            reader.BaseStream.Position = 0;
+            int indexOfCommand = 0;
+            mockedReader.Setup(r => r.ReadLine()).Returns(() => moveCommands[indexOfCommand].ToString());
 
             bool actual;
-
-            for (int i = 0; i < numberCommands.Length; i++)
+            for (; indexOfCommand < moveCommands.Count; indexOfCommand++)
             {
                 interf.GetCommandFromInput();
                 result = interf.GetArgumentValue(GlobalConstants.DestinationTileValue);
-                actual = (result as int? == int.Parse(numberCommands[i]));
+                actual = (result as int? == moveCommands[indexOfCommand]);
                 Assert.IsTrue(actual);
             }
+
+            mockedReader.Verify(r => r.ReadLine(), Times.Exactly(moveCommands.Count));
         }
 
         [TestMethod]
         public void TestGetCommandFromInputToReturnInvalidCommand()
         {
-            StreamReader reader = new StreamReader("../../testCommands/UndefinedTextCommand.txt");
-            Console.SetIn(reader);
+            var mockedReader = new Mock<TextReader>();
+            Console.SetIn(mockedReader.Object);
 
-            ConsoleInterface userInterface = new ConsoleInterface();
+            IList<string> undefindedCommands =
+                new List<string>() { "Undefined!", "Fake", "No Hack", "CHeat", "beer" };
 
-            var invalidComomands = reader.ReadToEnd().Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            IUserInterface userInterface = new ConsoleInterface();
 
-            reader.DiscardBufferedData();
-            reader.BaseStream.Seek(0, SeekOrigin.Begin);
-            reader.BaseStream.Position = 0;
+            int indexOfCommand = 0;
+            mockedReader.Setup(r => r.ReadLine()).Returns(() => undefindedCommands[indexOfCommand]);
 
-            for (int i = 0; i < invalidComomands.Length; i++)
+            for (; indexOfCommand < undefindedCommands.Count; indexOfCommand++)
             {
                 Assert.AreEqual(Command.Invalid, userInterface.GetCommandFromInput());
             }
+
+            mockedReader.Verify(r => r.ReadLine(), Times.Exactly(undefindedCommands.Count));
         }
 
         [TestMethod]
         public void TestGetCommandFromInputToReturnInvalidCommandWhenInvalidStyleCommandIsWrong()
         {
-            StreamReader reader = new StreamReader("../../testCommands/WrongStyleCommands.txt");
-            Console.SetIn(reader);
+            var mockedReader = new Mock<HelperReader>();
+            IList<String> invalidStyleCommands =
+                new List<string>() { "style=Sod=sd", " styles", "style solid", "style=set=solid", "set style=solid", "solidStyle" };
 
-            ConsoleInterface userInterface = new ConsoleInterface();
+            IUserInterface userInterface = new ConsoleInterface();
+            Console.SetIn(mockedReader.Object);
 
-            var numberCommands = reader.ReadToEnd().Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            int indexOfCommand = 0;
+            mockedReader.Setup(r => r.ReadLine()).Returns(() => invalidStyleCommands[indexOfCommand]);
 
-            reader.DiscardBufferedData();
-            reader.BaseStream.Seek(0, SeekOrigin.Begin);
-            reader.BaseStream.Position = 0;
-
-            for (int i = 0; i < numberCommands.Length; i++)
+            for (; indexOfCommand < invalidStyleCommands.Count; indexOfCommand++)
             {
                 Assert.AreEqual(Command.Invalid, userInterface.GetCommandFromInput());
             }
+
+            mockedReader.Verify(r => r.ReadLine(), Times.Exactly(invalidStyleCommands.Count));
         }
 
         [TestMethod]
         public void TestGetCommandFromInputToReturnCorrectCommands()
         {
-            StreamReader reader = new StreamReader("../../testCommands/CorrectCommands.txt");
-            Console.SetIn(reader);
+            var mock = new Mock<HelperReader>();
+            Console.SetIn(mock.Object);
+            IList<string> commandstoPr =
+                 new List<string>() { "start", "how", "top", "save", "load", "restart", "exit", "menu", "solve", "restart", "exit" };
 
-            ConsoleInterface userInterface = new ConsoleInterface();
+            int index = 0;
+            mock.Setup(r => r.ReadLine()).Returns(() => commandstoPr[index]);
 
-            var numberCommands = reader.ReadToEnd().Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
-
-            reader.DiscardBufferedData();
-            reader.BaseStream.Seek(0, SeekOrigin.Begin);
-            reader.BaseStream.Position = 0;
-
-            
+            IUserInterface userInterface = new ConsoleInterface();
             string correctCommand;
-            for (int i = 0; i < numberCommands.Length; i++)
+            for (; index < commandstoPr.Count; index++)
             {
-                correctCommand = (numberCommands[i][0].ToString().ToUpper() + numberCommands[i].Substring(1));
+                correctCommand = (commandstoPr[index][0].ToString().ToUpper() + commandstoPr[index].Substring(1));
                 Command command = (Command)Enum.Parse(typeof(Command), correctCommand);
                 Assert.AreEqual(command, userInterface.GetCommandFromInput());
             }
+
+            mock.Verify(r => r.ReadLine(), Times.Exactly(commandstoPr.Count));
         }
 
     }
