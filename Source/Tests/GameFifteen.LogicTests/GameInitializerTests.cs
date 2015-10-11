d95062d9371aafead600ed6d8d61aa987db98b67
M-Yankov <pointblank12345@abv.bg>
2015-10-12 00:51:49 +0300

    FInally the Engine.cs is screwed up and now it is tested.

--- Source/Tests/GameFifteen.LogicTests/GameInitializerTests.cs
+++ Source/Tests/GameFifteen.LogicTests/GameInitializerTests.cs
@@ -13,6 +13,7 @@ namespace GameFifteen.LogicTests
     using GameFifteen.Common;
     using GameFifteen.Console;
     using GameFifteen.Console.Styles;
+    using GameFifteen.ConsoleTests;
 
     /// <summary>
     /// This class tests if the methods in GameFifteen.Logic module projects are successfully invoked.
@@ -77,5 +78,122 @@ namespace GameFifteen.LogicTests
             IGrid grid = new Grid();
             StandartGameFifteenEngine engine = new StandartGameFifteenEngine(rende, consoleInterface, gameInit, player, grid);
         }
+
+        [TestMethod]
+        public void TestThatThisEngineCannotBeTested()
+        {
+            var mockedConsole = new Mock<HelperReader>();
+            var mockedInterface = new Mock<IUserInterface>(MockBehavior.Loose);
+            mockedConsole.SetupSequence(c => c.ReadLine()).Returns("start").Returns("exit").Returns("no");
+            
+            Console.SetIn(mockedConsole.Object);
+
+            //mockedInterface.SetupSequence(i => i.GetCommandFromInput()).Returns(Command.Start).Returns(Command.Exit).Returns(Command.Yes);
+            IRenderer rende = new ConsoleRenderer(new BorderStyleFactory());
+            IUserInterface consoleInterface = new ConsoleInterface();
+            IGameInitializater gameInit = new StandartGameInitializer();
+            IPlayer player = new Player();
+            IGrid grid = new Grid();
+            StandartGameFifteenEngine engine = new StandartGameFifteenEngine(rende, consoleInterface, gameInit, player, grid);
+            engine.Initialize();
+        }
+
+        [TestMethod]
+        public void TestNormalBehaviorOnGameEngine()
+        {
+            var mockedConsole = new Mock<HelperReader>();
+            var mockedInterface = new Mock<IUserInterface>(MockBehavior.Loose);
+            mockedConsole.SetupSequence(c => c.ReadLine()).Returns("start").Returns("solve").Returns("pesho").Returns("yes").Returns("style=double").Returns("exit").Returns("no");
+
+            Console.SetIn(mockedConsole.Object);
+
+            IRenderer rende = new ConsoleRenderer(new BorderStyleFactory());
+            IUserInterface consoleInterface = new ConsoleInterface();
+            IGameInitializater gameInit = new StandartGameInitializer();
+            IPlayer player = new Player();
+            IGrid grid = new Grid();
+            StandartGameFifteenEngine engine = new StandartGameFifteenEngine(rende, consoleInterface, gameInit, player, grid);
+            engine.Initialize();
+        }
+
+        [TestMethod]
+        public void TestEngineToSaveScoreFromPlayerAndPrintIt()
+        {
+            var mockedConsole = new Mock<HelperReader>();
+            var mockedWriter = new Mock<HelperWriter>();
+            var mockedInterface = new Mock<IUserInterface>(MockBehavior.Loose);
+            mockedConsole.SetupSequence(c => c.ReadLine())
+                .Returns("start")
+                .Returns("solve")
+                .Returns("pesho")
+                .Returns("yes")
+                .Returns("1")
+                .Returns("2")
+                .Returns("3")
+                .Returns("4")
+                .Returns("save")
+                .Returns("5")
+                .Returns("5")
+                .Returns("6")
+                .Returns("restart")
+                .Returns("7")
+                .Returns("8")
+                .Returns("9")
+                .Returns("10")
+                .Returns("11")
+                .Returns("load")
+                .Returns("yes")
+                .Returns("12")
+                .Returns("14")
+                .Returns("13")
+                .Returns("solve")
+                .Returns("pesho")
+                .Returns("yes")
+                .Returns("top")
+                .Returns("game")
+                .Returns("exit")
+                .Returns("no");
+
+            Console.SetIn(mockedConsole.Object);
+            Console.SetOut(mockedWriter.Object);
+            IRenderer rende = new ConsoleRenderer(new BorderStyleFactory());
+            IUserInterface consoleInterface = new ConsoleInterface();
+            IGameInitializater gameInit = new StandartGameInitializer();
+            IPlayer player = new Player();
+            IGrid grid = new Grid();
+            StandartGameFifteenEngine engine = new StandartGameFifteenEngine(rende, consoleInterface, gameInit, player, grid);
+            engine.Initialize();
+
+            mockedWriter.Verify(w => w.Write(It.Is<string>(s => s == "pesho ")), Times.AtLeast(2));
+        }
+
+        [TestMethod]
+        public void TestEngineToWhenHowCommandIsPassedTPrintDescription()
+        {
+            var mockedConsole = new Mock<HelperReader>();
+            var mockedWriter = new Mock<HelperWriter>();
+            var mockedInterface = new Mock<IUserInterface>(MockBehavior.Loose);
+            mockedConsole.SetupSequence(c => c.ReadLine())
+                .Returns("start")
+                .Returns("how")
+                .Returns("game")
+                .Returns("load")
+                .Returns("yes")
+                .Returns("exit")
+                .Returns("no");
+
+            Console.SetIn(mockedConsole.Object);
+            Console.SetOut(mockedWriter.Object);
+            IRenderer rende = new ConsoleRenderer(new BorderStyleFactory());
+            IUserInterface consoleInterface = new ConsoleInterface();
+            IGameInitializater gameInit = new StandartGameInitializer();
+            IPlayer player = new Player();
+            IGrid grid = new Grid();
+            StandartGameFifteenEngine engine = new StandartGameFifteenEngine(rende, consoleInterface, gameInit, player, grid);
+            engine.Initialize();
+
+            mockedWriter.Verify(w => w.Write(It.Is<string>(s => s.Contains("->"))), Times.AtLeast(GameMessages.CommandsDescription.Count));
+        }
+
     }
 }
